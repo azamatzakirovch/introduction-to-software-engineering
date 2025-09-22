447d67d0a1579a5e07c905adc6c1a30e66ba0c9d
Azamat Zakirovch <a.eshqoziyev@newuu.uz>
2025-09-22 15:17:12 +0500

    some filed edited  - TranscriptManager.ts file is edited. New class created with the name of "TranscriptManager". There are 2 .json files imported. they are student_data.json and student_transcript.json file. © Azamat E. Zakirovch

--- assignment-1/src/TranscriptManager.ts
+++ assignment-1/src/TranscriptManager.ts
@@ -1,6 +1,17 @@
 import students_data from "./../student_data.json"
-import transcripts_data from "./../student_transcript.json"
+import students_transcript from "./../student_transcript.json"
 
+/*
+ - students_data is imported to get the database of students.
+ - students_transcript is imported to get the database of students.
+*/
+
+/*
+ - TranscriptManager class created to make REST API. Inside of this class CRUD operations written. I used encapsulation methods inorder to safety of students data.
+ - In home task slide professor said that I need to make initializer and make 4 default students data. I want to be a professional that's why I made transcripts and students database separately and saved them .json files.
+*/
 class TranscriptManager {
-
+    
 }
+
+export default new TranscriptManager();
