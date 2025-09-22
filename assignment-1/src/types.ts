284beeac9dd86d7d86a200dda2f9a87c9792ce95
Azamat Zakirovch <a.eshqoziyev@newuu.uz>
2025-09-22 16:08:56 +0500

    New file created  - types.ts in order to represent the costume data types. Inside the types.ts file I made a 3 new data types. It is possible to use them. You can check it my dear brother.
    © Azamat E. Zakirovch

---
+++ assignment-1/src/types.ts
@@ -0,0 +1,19 @@
+export type STUDENT = {
+    ID: number;
+    NAME: string;
+    LASTNAME: string;
+    EMAIL: string;
+    SCHOOL: string;
+    EDUCATION_PROGRAM: string;
+    PHONE: string;
+}
+
+export type GRADE = {
+    COURSE: string;
+    GRADE: number;
+}
+
+export type TRANSCRIPT = {
+    ID: number;
+    GRADES: GRADE[];
+}
