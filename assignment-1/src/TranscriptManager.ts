fc87d01822c5348c74434aa4143d8e094a6e18d0
Azamat Zakirovch <a.eshqoziyev@newuu.uz>
2025-09-22 16:09:12 +0500

    Editions  - TranscriptManager.ts file is edited. New methods and getters created. Encapsulation methods created right here. You can check what kind of functions created my dear brother.
    © Azamat E. Zakirovch

--- assignment-1/src/TranscriptManager.ts
+++ assignment-1/src/TranscriptManager.ts
@@ -1,5 +1,6 @@
 import students_data from "./../student_data.json"
 import students_transcript from "./../student_transcript.json"
+import {STUDENT, TRANSCRIPT} from "./types"
 
 /*
  - students_data is imported to get the database of students.
@@ -11,7 +12,33 @@ import students_transcript from "./../student_transcript.json"
  - In home task slide professor said that I need to make initializer and make 4 default students data. I want to be a professional that's why I made transcripts and students database separately and saved them .json files.
 */
 class TranscriptManager {
-    
+    private students : STUDENT[] = students_data;
+    private transcript : TRANSCRIPT[] = students_transcript;
+
+    private get_students() : STUDENT[] {
+        return this.students;
+    }
+
+    private get_transcript() : TRANSCRIPT[] {
+        return this.transcript;
+    }
+
+    public all_students() : STUDENT[] {
+        return this.get_students();
+    }
+
+    public all_transcripts() : TRANSCRIPT[] {
+        return this.get_transcript();
+    }
+
+    public get_student_by_id(identification : number) : STUDENT | undefined {
+        return this.get_students().find((student) => student.ID === identification);
+    }
+
+    public get_transcript_by_id(identification: number) : TRANSCRIPT | undefined {
+        return this.get_transcript().find((transcript) => transcript.ID === identification);
+    }
+
 }
 
 export default new TranscriptManager();
