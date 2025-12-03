5541fb51fd66ba5514a7c347e0bd0bbc69e4dd15
E-Zakirovich <azamateshqoziyev2@gmail.com>
2025-12-03 19:43:51 +0500

    I refactored 2 methods from Game.ts file. What I did is, I created 2 other files, in order to avoid code smells.

--- assignment-5/src/Game.ts
+++ assignment-5/src/Game.ts
@@ -66,36 +66,3 @@ export class Game {
         return ' ';
     }
 }
-
-interface Tile {
-    X: number;
-    Y: number;
-    Symbol: string;
-}
-
-class Board {
-    private _plays : Tile[] = [];
-
-    constructor()
-    {
-        for (let i = 0; i < 3; i++)
-        {
-            for (let j = 0; j < 3; j++)
-            {
-                const tile : Tile = {X :i, Y:j, Symbol:" "};
-                this._plays.push(tile);
-            }
-        }
-    }
-
-    public TileAt(x:  number, y: number): Tile {
-        return this._plays.find((t:Tile) => t.X == x && t.Y == y)!
-    }
-
-    public AddTileAt(symbol: string, x: number, y: number) : void
-    {
-        const tile : Tile = {X :x, Y:y, Symbol:symbol};
-
-        this._plays.find((t:Tile) => t.X == x && t.Y == y)!.Symbol = symbol;
-    }
-}
---
+++ assignment-5/src/Tile.ts
@@ -0,0 +1,5 @@
+interface Tile {
+    X: number;
+    Y: number;
+    Symbol: string;
+}
